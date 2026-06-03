8f96d8391e6b984ebe6b703331c7c80b837b15f9
rishith-git <padisala30@gmail.com>
2026-06-03 10:14:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#Printing a Greeting Message
+print("Hello World!")
+
+#Printing my Name
+print("My name is Rishith")
+
+#Printing my daily routine step by step
+print("1. Wake up at 5:00 AM")
+print("2. Brush my teeth")
+print("3. Leave for playing badminton")
+print("4. Come back home at 7:30 AM")
+print("5. Finish my daily chores")
+print("6. Have breakfast")
+print("7. Start my studies at 9:00 AM")
+print("8. Take a break at 11:00 AM")
+print("9. Helping my parents with household work")
+print("10. Have lunch at 1:00 PM")
+print("11. Watch TV for an hour with my family")
+print("12. Take a nap at 3:00 PM")
+print("13. Continue my studies at 4:00 PM")
+print("14. Halt my studies at 7:00 PM")
+print("15. Have dinner at 7:30 PM")
+print("16. Spend time with my family")
+print("17. Read a book for half an hour")
+print("18. Go to bed at 10:00 PM")
+      
+#Printing my total time which I studied in a day
+print("I studied for a total of 2 hours in the morning and 3 hours in the evening, which makes it a total of 5 hours of study in a day.")
+print("sum of 2 hours and 3 hours is", 2+3, "hours")
+
+#Printing the total time I spent on studying in a week
+print("In a week, I studied for a total of", 5*7, "hours.")
+
+#Printing the total time I spent on studying in a month
+print("In a month, I studied for a total of", 5*30, "hours.")
+
+#Printing the total time I spent on studying in a year
+print("In a year, I studied for a total of", 5*365, "hours.")
