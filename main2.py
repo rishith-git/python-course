65d98a8ef4c25653ce8950c46a21cfc12fb8754f
rishith-git <padisala30@gmail.com>
2026-06-03 10:48:07 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,10 @@
+#Printing a welcome message
+print("Hello, welcome to programming!")
+#Printing a number
+print(4)
+#Printing a stream
+print("This is a stream of text." "\n")
+#Printing multiple arguments
+print("The values included are:", 2.35,5,5.4)
+#Printing using end parameter
+print("This is the final part of the code.", end=" ")
