e23913adfcdc59fc45e8300c677c4a80ae272a2c
rishith-git <padisala30@gmail.com>
2026-06-03 11:38:35 +0530

    Add files via upload

--- main02.py
+++ main02.py
@@ -1,16 +1,18 @@
-#Printing the variables and storing birthday dates in them
+#Denoting the variables and storing names in them
 name1 = "Rishith"
 name2 = "Roshan"
 name3 = "Deepak"
 name4 = "Sanjay"
 name5 = "Suresh"
 
+#Denoting the variables and storing birthday dates in them
 bday1 = X = "25-06-2009"
 bday2 = Y = "16-06-2012"
 bday3 = Z = "20-10-2012"
 bday4 = A = "15-11-2009"
 bday5 = B = "30-12-2002"
 
+#Printing the names and their correspnding birthdays
 print(name1, "was born on", bday1)
 print(name2, "was born on", bday2)
 print(name3, "was born on", bday3)
