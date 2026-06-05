5cde4f1a949ed19c7f8e3be070d05dc2cdde71d8
rishith-git <padisala30@gmail.com>
2026-06-05 10:51:12 +0530

    Add files via upload

---
+++ main8.py
@@ -0,0 +1,14 @@
+#Printing the length of the string
+
+str = "abcdefghi"
+print(len(str))
+
+#Indexing and Slicing the string
+
+str = "Coding"
+str1 = "Python"
+str2 = "Programming" 
+
+print(str[0:3])
+print(str1[0:6:2])
+print(str2[1:-1:3])
