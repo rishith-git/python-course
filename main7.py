c411161cff43f644d3619d36ef3bc8d46a9322d1
rishith-git <padisala30@gmail.com>
2026-06-05 10:50:38 +0530

    Add files via upload

---
+++ main7.py
@@ -0,0 +1,24 @@
+#Type casting different data types
+
+name = "Rishith"
+print("The type of name is ", type(name))
+
+age = 17
+print("The type of age is ", type(age))
+
+weight = 60.5
+print("The type of weight is ", type(weight))
+
+is_student = True
+print("The type of is_student is", type(is_student))
+
+#Type casting
+
+age = str(age)
+print("The type of age after type casting is ", type(age))
+
+weight = int(weight)
+print("The type of weight after type casting is ", type(weight))
+
+is_student = str(is_student)
+print("The type of is_student after type casting is ", type(is_student))
