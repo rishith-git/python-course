8d38ce20769139d8c3ddf3616b99953c8141d854
rishith-git <padisala30@gmail.com>
2026-06-05 10:49:49 +0530

    Add files via upload

---
+++ main6.py
@@ -0,0 +1,16 @@
+#Printing the equivalent data types
+
+a = 10 
+print("type of a is ", type(a))
+
+b = 10.5
+print("type of b is ", type(b))
+
+x = True
+print("type of x is ", type(x))
+
+y = "Hello"
+print("type of y is", type(y))
+
+z = "123"
+print("type of z is", type(z))
