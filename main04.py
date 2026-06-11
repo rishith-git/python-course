3c6bd0c90c084c1f10eebacd994cca915cbfc91c
rishith-git <padisala30@gmail.com>
2026-06-11 17:43:22 +0530

    Add files via upload

--- main04.py
+++ main04.py
@@ -1,7 +1,19 @@
-print("Enter your name:")
-name = input()
-print("Hello, " + name + "!")
+#Printing the square roots of the perfect square numbers
 
-#Printing a congratulations message
-print("Congratulations, " + name + "!")
-print(" Iam glad that you have won a gold medal in the competition.")
+x = 4
+y = 9
+z = 16
+
+print("The square root of", x, "is", x**0.5)
+print("The square root of", y, "is", y**0.5)
+print("The square root of", z, "is", z**0.5)
+
+#Printing the square root of imperfect square numbers
+
+p = 5
+q = 11
+r = 21
+
+print("The square root of", p, "is", p**0.5)
+print("The square root of", q, "is", q**0.5)
+print("The square root of", r, "is", r**0.5)
