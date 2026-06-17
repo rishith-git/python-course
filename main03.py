e9a61a4f63c9b4b6396b7dd168ee52852f09f14b
rishith-git <padisala30@gmail.com>
2026-06-17 09:35:35 +0530

    Add files via upload

---
+++ main03.py
@@ -0,0 +1,7 @@
+print("Enter your name:")
+name = input()
+print("Hello, " + name + "!")
+
+#Printing a congratulations message
+print("Congratulations, " + name + "!")
+print(" Iam glad that you have won a gold medal in the competition.")
