936416e52a548942af5ab159ee91748ccf8a01b6
rishith-git <padisala30@gmail.com>
2026-07-01 13:27:10 +0530

    Add files via upload

---
+++ main07.py
@@ -0,0 +1,8 @@
+age = int(input("Please enter your age: "))
+
+if 10 <age< 20:
+    print("You are a teenager.")
+elif age >= 20:
+    print("You are an adult.")
+else:
+    print("You are a child.")
