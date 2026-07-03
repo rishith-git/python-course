2a51047d07402a9881be53dc711662ef7888abfd
rishith-git <padisala30@gmail.com>
2026-07-03 19:59:57 +0530

    Add files via upload

--- main04.py
+++ main04.py
@@ -1,19 +1,8 @@
-#Printing the square roots of the perfect square numbers
+#ASCII value checker
+char = input("Enter a character:")
+if len(char) == 1:
+    ascii_value = ord(char)
+    print(f"The ASCII value of '{char}' is: {ascii_value}")
+else:
+    print("Please enter a single character.")
 
-x = 4
-y = 9
-z = 16
-
-print("The square root of", x, "is", x**0.5)
-print("The square root of", y, "is", y**0.5)
-print("The square root of", z, "is", z**0.5)
-
-#Printing the square root of imperfect square numbers
-
-p = 5
-q = 11
-r = 21
-
-print("The square root of", p, "is", p**0.5)
-print("The square root of", q, "is", q**0.5)
-print("The square root of", r, "is", r**0.5)
